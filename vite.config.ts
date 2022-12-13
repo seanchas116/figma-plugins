1ba198d8e016ad1a010ff8b08b5679a21196299f
Ryohei Ikegami <iofg2100@gmail.com>
2022-12-13 17:25:32 +0900

    Add vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, Plugin, ViteDevServer } from "vite";
+import react from "@vitejs/plugin-react";
+
+const plugin = (): Plugin => {
+  return {
+    name: "vite-plugin-component-catalog",
+    configureServer(server: ViteDevServer) {
+      // other html handled after vite's inner middlewares.
+      return () => {
+        server.middlewares.use("/", async (req, res, next) => {
+          if (req.originalUrl !== "/") {
+            return next();
+          }
+
+          const html = `
+          <html>
+            <head>
+              <title>My App</title>
+            </head>
+            <body>
+            <h1>Replace index.html</h1>
+            </body>
+          </html>
+        `;
+
+          res.end(await server.transformIndexHtml("/", html));
+          next();
+        });
+      };
+    },
+  };
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), plugin()],
+});
