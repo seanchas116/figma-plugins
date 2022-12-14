f8131aec84c3db34b32246d0f5b105b67ca656cf
Ryohei Ikegami <iofg2100@gmail.com>
2022-12-14 16:42:57 +0900

    Support transparency

--- packages/electron/src/main.ts
+++ packages/electron/src/main.ts
@@ -8,6 +8,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    transparent: true,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
