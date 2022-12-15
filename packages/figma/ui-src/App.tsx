7239f5ede7f401dccf92d259a3d6782bd7f98cf2
Ryohei Ikegami <iofg2100@gmail.com>
2022-12-15 16:22:40 +0900

    Add form

--- packages/figma/ui-src/App.tsx
+++ packages/figma/ui-src/App.tsx
@@ -52,6 +52,19 @@ export const App: React.FC = () => {
         <option value="Checkbox">Checkbox</option>
         <option value="TextField">TextField</option>
       </select>
+      <dl className="flex flex-col gap-1">
+        <dt className="text-gray-500">Primary</dt>
+        <dd>
+          <input className="accent-indigo-500" type="checkbox" />
+        </dd>
+        <dt className="text-gray-500">Label</dt>
+        <dd>
+          <input
+            className="border border-gray-300 rounded-md shadow-sm py-1 px-1 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            type="text"
+          />
+        </dd>
+      </dl>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={handleClick}
