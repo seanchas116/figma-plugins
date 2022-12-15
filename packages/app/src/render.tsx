f4894c6457a67a82ee67dfed99b6f44a0169977f
Ryohei Ikegami <iofg2100@gmail.com>
2022-12-15 15:26:14 +0900

    Render timestamp (as debug)

--- packages/app/src/render.tsx
+++ packages/app/src/render.tsx
@@ -2,6 +2,33 @@ import * as htmlToImage from "html-to-image";
 import { Button } from "./stories/Button";
 import ReactDOMClient from "react-dom/client";
 
+const root = document.getElementById("root") as HTMLElement;
+const reactRoot = ReactDOMClient.createRoot(root);
+
+async function renderComponent(
+  node: JSX.Element,
+  options: {
+    width: number;
+    height: number;
+  }
+): Promise<ArrayBuffer> {
+  reactRoot.render(node);
+  await new Promise((resolve) => setTimeout(resolve, 0));
+
+  console.time("htmlToImage");
+  const pngURL = await htmlToImage.toPng(
+    root.firstElementChild as HTMLElement,
+    {
+      width: options.width,
+      height: options.height,
+    }
+  );
+  const pngBuffer = await fetch(pngURL).then((res) => res.arrayBuffer());
+  console.timeEnd("htmlToImage");
+
+  return pngBuffer;
+}
+
 const onMessage = async (event: MessageEvent) => {
   if (event.data.type !== "iframe:render") {
     return;
@@ -9,26 +36,13 @@ const onMessage = async (event: MessageEvent) => {
 
   console.log(event.data);
 
-  const root = document.createElement("div");
-  document.body.append(root);
-
-  const reactRoot = ReactDOMClient.createRoot(root);
-  reactRoot.render(<Button primary label="Button" />);
-
-  await new Promise((resolve) => requestIdleCallback(resolve));
-
-  console.time("htmlToImage");
-  const pngURL = await htmlToImage.toPng(
-    root.firstElementChild as HTMLElement,
+  const pngBuffer = await renderComponent(
+    <Button primary label={Date.now().toString()} />,
     {
       width: event.data.width,
       height: event.data.height,
     }
   );
-  const pngBuffer = await fetch(pngURL).then((res) => res.arrayBuffer());
-  console.timeEnd("htmlToImage");
-
-  root.remove();
 
   window.parent.postMessage(
     {
