a4238c13fb3647cee51b05763a6690e6e84893c1
Ryohei Ikegami <iofg2100@gmail.com>
2022-12-15 16:13:02 +0900

    Add select box

--- packages/figma/ui-src/App.tsx
+++ packages/figma/ui-src/App.tsx
@@ -45,7 +45,13 @@ export const App: React.FC = () => {
   }, []);
 
   return (
-    <div className="p-2 flex flex-col gap-2">
+    <div className="p-2 flex flex-col gap-2 text-xs">
+      <select className="border border-gray-300 rounded-md shadow-sm py-1 px-1 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
+        <option value="">Not Attached</option>
+        <option value="Button">Button</option>
+        <option value="Checkbox">Checkbox</option>
+        <option value="TextField">TextField</option>
+      </select>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={handleClick}
