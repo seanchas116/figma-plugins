d97671fade812c83f1b9dfeefaf8eb36662a82d0
Ryohei Ikegami <iofg2100@gmail.com>
2022-12-16 16:56:56 +0900

    WIP size constraint icons

--- packages/figma/ui-src/App.tsx
+++ packages/figma/ui-src/App.tsx
@@ -4,6 +4,48 @@ import { useEffect, useState } from "preact/hooks";
 import { MessageToUI } from "../message";
 import { ComponentState } from "../data";
 
+const AutoWidthIcon = () => {
+  return (
+    <svg width="16" height="16" viewBox="0 0 16 16">
+      <path
+        d="M2 8H14M2 8L4 10M2 8L4 6M14 8L12 6M14 8L12 10"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+const AutoHeightIcon = () => {
+  return (
+    <svg width="16" height="16" viewBox="0 0 16 16">
+      <path
+        d="M2.5 4.5H13.5M2.5 8H13.5M2.5 11.5H8"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+const FixedSizeIcon = () => {
+  return (
+    <svg width="16" height="16" viewBox="0 0 16 16">
+      <rect
+        x="2.5"
+        y="2.5"
+        width="11"
+        height="11"
+        rx="2"
+        stroke="currentColor"
+        fill="none"
+      />
+    </svg>
+  );
+};
+
 export const App: React.FC = () => {
   const [component, setComponent] = useState<ComponentState | undefined>();
 
@@ -51,6 +93,20 @@ export const App: React.FC = () => {
         <option value="Checkbox">Checkbox</option>
         <option value="TextField">TextField</option>
       </select>
+      <div>
+        <button
+          className="p-0.5 rounded aria-selected:bg-blue-500 aria-selected:text-white"
+          aria-selected
+        >
+          <AutoWidthIcon />
+        </button>
+        <button className="p-0.5 rounded aria-selected:bg-blue-500 aria-selected:text-white">
+          <AutoHeightIcon />
+        </button>
+        <button className="p-0.5 rounded aria-selected:bg-blue-500 aria-selected:text-white">
+          <FixedSizeIcon />
+        </button>
+      </div>
       <dl className="flex flex-col gap-1">
         <dt className="text-gray-500">Primary</dt>
         <dd>
