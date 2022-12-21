ac405eb5403fd5c11702d373cf53f4c123685063
Ryohei Ikegami <iofg2100@gmail.com>
2022-12-21 18:30:57 +0900

    Add tokens

---
+++ packages/app/src/tokens.ts
@@ -0,0 +1,24 @@
+export const colors = {
+  background: "#fff",
+  text: "#333",
+  primary: "#1ea7fd",
+  border: "#rgba(0, 0, 0, 0.15)",
+};
+
+export const textStyles = {
+  heading: {
+    fontSize: 24,
+    fontWeight: 600,
+    lineHeight: 1.25,
+  },
+  body: {
+    fontSize: 16,
+    fontWeight: 400,
+    lineHeight: 1.5,
+  },
+  small: {
+    fontSize: 14,
+    fontWeight: 400,
+    lineHeight: 1.5,
+  },
+};
