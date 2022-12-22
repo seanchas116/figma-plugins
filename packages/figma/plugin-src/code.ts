2ff7942ee4b12da74d0df166d6565fe350d75c20
Ryohei Ikegami <iofg2100@gmail.com>
2022-12-22 16:58:19 +0900

    Rename to UIFlux

--- packages/figma/plugin-src/code.ts
+++ packages/figma/plugin-src/code.ts
@@ -15,7 +15,7 @@ import { debounce, findFontForWeight, postMessageToUI } from "./common";
 import { onRenderDone, renderInstance, renderInstanceImage } from "./render";
 import parseCSSColor from "parse-css-color";
 
-const productName = "Mixlog";
+const productName = "UIFlux";
 
 figma.showUI(__html__, { width: 240, height: 240 });
 
