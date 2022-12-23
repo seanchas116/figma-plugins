8edbdf70ffbd0d0130afec0f4ed82612e2c5250e
Ryohei Ikegami <iofg2100@gmail.com>
2022-12-23 18:10:05 +0900

    WIP

--- packages/inspect/components/Inspector.tsx
+++ packages/inspect/components/Inspector.tsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 export const Inspector: React.FC = () => {
+  const [data, setData] = useState("TODO");
+
   return (
-    <div>
+    <section>
       <h1 className="text-2xl font-bold">Inspector</h1>
-    </div>
+      <dl>
+        <dt>Figma Access Token</dt>
+        <dd>
+          <input className="border border-gray-300 rounded-md shadow-sm py-1 px-1 focus:outline-none focus:ring-blue-500 focus:border-blue-500" />
+        </dd>
+        <dt>File URL</dt>
+        <dd>
+          <input className="border border-gray-300 rounded-md shadow-sm py-1 px-1 focus:outline-none focus:ring-blue-500 focus:border-blue-500" />
+        </dd>
+      </dl>
+      <pre>{data}</pre>
+    </section>
   );
 };
