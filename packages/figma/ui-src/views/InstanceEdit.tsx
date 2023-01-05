72dc8afdb3460e29861196b30052132314dfc712
Ryohei Ikegami <iofg2100@gmail.com>
2023-01-05 16:14:20 +0900

    Fix

--- packages/figma/ui-src/views/InstanceEdit.tsx
+++ packages/figma/ui-src/views/InstanceEdit.tsx
@@ -27,10 +27,10 @@ export const InstanceEdit: FunctionComponent = () => {
 
   return (
     <div className="flex flex-col gap-3">
-      <h1 className="font-semibold text-xs my-1">
+      <h1 className="font-semibold text-xs">
         {componentDoc?.name ?? "Component Not Found"}
       </h1>
-      <div className="flex -my-1">
+      <div className="flex">
         <SizingButton
           title="Auto Width"
           aria-selected={instance.autoResize === "widthHeight"}
