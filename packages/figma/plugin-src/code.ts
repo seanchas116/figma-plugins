27aa3baca8287fbcd177b7be43ebdb8d3a2b29a8
Ryohei Ikegami <iofg2100@gmail.com>
2023-01-09 16:48:35 +0900

    Save plugin size

--- packages/figma/plugin-src/code.ts
+++ packages/figma/plugin-src/code.ts
@@ -13,6 +13,10 @@ import { getElementIR } from "./codegen/element";
 import { Component } from "@uimix/element-ir";
 import { getComponentIRs } from "./codegen/component";
 
+figma.clientStorage.getAsync("size").then((size) => {
+  if (size) figma.ui.resize(size.width, size.height);
+});
+
 figma.showUI(__html__, { width: 240, height: 240 });
 
 const onDocumentChange = debounce((event: DocumentChangeEvent) => {
@@ -117,6 +121,7 @@ class RPCHandler implements IUIToPluginRPC {
   }
   async resize(width: number, height: number): Promise<void> {
     figma.ui.resize(width, height);
+    figma.clientStorage.setAsync("size", { width, height });
   }
 
   async exportWholeDocument(): Promise<Component[]> {
