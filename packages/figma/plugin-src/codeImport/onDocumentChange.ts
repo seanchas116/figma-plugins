bfc95247222d50a9d35ec2bbbeec0f54d8f5cc3a
Ryohei Ikegami <iofg2100@gmail.com>
2023-01-14 09:58:27 +0900

    Refactor

--- packages/figma/plugin-src/codeImport/onDocumentChange.ts
+++ packages/figma/plugin-src/codeImport/onDocumentChange.ts
@@ -8,51 +8,50 @@ import {
 import { debounce } from "../util/common";
 import { renderInstance } from "./render";
 
-function handleCodeInstanceResize(change: DocumentChange) {
-  if (
-    change.type === "PROPERTY_CHANGE" &&
-    change.node.type === "INSTANCE" &&
-    !change.node.removed &&
-    (change.properties.includes("width") ||
-      change.properties.includes("height"))
-  ) {
-    const node = change.node;
-    const instance = getInstanceInfo(node);
-    if (!instance) {
-      return;
-    }
-
-    const renderedSize = getRenderedSize(node);
-    if (
-      renderedSize &&
-      renderedSize.width === node.width &&
-      renderedSize.height === node.height
-    ) {
-      return;
-    }
-
-    if (instance.autoResize !== "none") {
-      const newAutoResize = change.properties.includes("height")
-        ? "none"
-        : "height";
-
-      const newInstanceInfo: CodeInstanceInfo = {
-        ...instance,
-        autoResize: newAutoResize,
-      };
-
-      setInstanceParams(node, newInstanceInfo);
-
-      onSelectionChange();
-    }
-
-    renderInstance(node);
+function handleCodeInstanceResize(node: InstanceNode, change: PropertyChange) {
+  const instance = getInstanceInfo(node);
+  if (!instance) {
+    return;
   }
+
+  const renderedSize = getRenderedSize(node);
+  if (
+    renderedSize &&
+    renderedSize.width === node.width &&
+    renderedSize.height === node.height
+  ) {
+    return;
+  }
+
+  if (instance.autoResize !== "none") {
+    const newAutoResize = change.properties.includes("height")
+      ? "none"
+      : "height";
+
+    const newInstanceInfo: CodeInstanceInfo = {
+      ...instance,
+      autoResize: newAutoResize,
+    };
+
+    setInstanceParams(node, newInstanceInfo);
+
+    onSelectionChange();
+  }
+
+  renderInstance(node);
 }
 
 const onDocumentChange = debounce(async (event: DocumentChangeEvent) => {
   for (const change of event.documentChanges) {
-    handleCodeInstanceResize(change);
+    if (
+      change.type === "PROPERTY_CHANGE" &&
+      change.node.type === "INSTANCE" &&
+      !change.node.removed &&
+      (change.properties.includes("width") ||
+        change.properties.includes("height"))
+    ) {
+      handleCodeInstanceResize(change.node, change);
+    }
   }
 }, 200);
 
