32b3d03756f012a37b857ba746c6a80420ccf96a
Ryohei Ikegami <iofg2100@gmail.com>
2023-01-22 16:09:30 +0900

    Refactor

--- packages/figma/plugin-src/code.ts
+++ packages/figma/plugin-src/code.ts
@@ -1,38 +1,7 @@
-import { DropMetadata } from "../types/data";
 import "./codeImport/onDocumentChange";
 //import "./responsive/onDocumentChange";
 import "./onSelectionChange";
-import { setIconPluginData } from "./pluginData";
+import "./onDrop";
 import "./rpc";
 
 figma.showUI(__html__, { width: 240, height: 240 });
-
-// Receive the drop event from the UI
-// @ts-ignore
-figma.on("drop", (event: DropEvent) => {
-  console.log(event);
-
-  const { files, node, dropMetadata } = event;
-  const iconDropMetadata = dropMetadata as DropMetadata;
-
-  if (files.length > 0 && files[0].type === "image/svg+xml") {
-    files[0].getTextAsync().then((text) => {
-      const newNode = figma.createNodeFromSvg(text);
-
-      if (node.type !== "DOCUMENT" && "appendChild" in node) {
-        node.appendChild(newNode);
-      }
-
-      setIconPluginData(newNode, {
-        name: iconDropMetadata.name,
-      });
-      newNode.name = iconDropMetadata.name;
-      newNode.x = event.x;
-      newNode.y = event.y;
-
-      figma.currentPage.selection = [newNode];
-    });
-
-    return false;
-  }
-});
---
+++ packages/figma/plugin-src/onDrop.ts
@@ -0,0 +1,31 @@
+import { DropMetadata } from "../types/data";
+import { setIconPluginData } from "./pluginData";
+
+// @ts-ignore
+figma.on("drop", (event: DropEvent) => {
+  console.log(event);
+
+  const { files, node, dropMetadata } = event;
+  const iconDropMetadata = dropMetadata as DropMetadata;
+
+  if (files.length > 0 && files[0].type === "image/svg+xml") {
+    files[0].getTextAsync().then((text) => {
+      const newNode = figma.createNodeFromSvg(text);
+
+      if (node.type !== "DOCUMENT" && "appendChild" in node) {
+        node.appendChild(newNode);
+      }
+
+      setIconPluginData(newNode, {
+        name: iconDropMetadata.name,
+      });
+      newNode.name = iconDropMetadata.name;
+      newNode.x = event.x;
+      newNode.y = event.y;
+
+      figma.currentPage.selection = [newNode];
+    });
+
+    return false;
+  }
+});
