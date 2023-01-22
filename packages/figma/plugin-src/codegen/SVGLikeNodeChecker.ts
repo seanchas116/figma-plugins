9f1684db2002486f47a92c93d59846e5627bb06e
Ryohei Ikegami <iofg2100@gmail.com>
2023-01-22 16:44:54 +0900

    Improve SVGLikeNodeChecker

--- packages/figma/plugin-src/codegen/SVGLikeNodeChecker.ts
+++ packages/figma/plugin-src/codegen/SVGLikeNodeChecker.ts
@@ -1,3 +1,5 @@
+import { getIconPluginData } from "../pluginData";
+
 const vectorLikeTypes: SceneNode["type"][] = [
   "LINE",
   "RECTANGLE",
@@ -16,6 +18,16 @@ class SVGLikeNodeChecker {
     if (memo != null) {
       return memo;
     }
+
+    if (getIconPluginData(node)) {
+      return true;
+    }
+    if (node.getSharedPluginData("iconify", "props")) {
+      // Looks like from Iconify plugin
+      // https://github.com/iconify/iconify-figma/blob/81562725aa663ad9b22278c4ade8cf24f9e8312b/src/figma/functions/set-icon-data.ts#L23
+      return true;
+    }
+    // TODO: check if node is from Material Symbols plugin
 
     if (vectorLikeTypes.includes(node.type)) {
       return true;
