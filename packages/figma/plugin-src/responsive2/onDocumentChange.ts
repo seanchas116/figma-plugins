0ef26d298afb37135bf4dc55ac5e2e98e12d4f60
Ryohei Ikegami <iofg2100@gmail.com>
2023-01-26 23:47:48 +0900

    WIP onDocumentChange

--- packages/figma/plugin-src/responsive2/onDocumentChange.ts
+++ packages/figma/plugin-src/responsive2/onDocumentChange.ts
@@ -1,5 +1,28 @@
-const onDocumentChange = async (event: DocumentChangeEvent) => {
-  // TODO
+import { getResponsiveArtboardData } from "../pluginData";
+import { getArtboard } from "./getArtboard";
+
+const onDocumentChange = (event: DocumentChangeEvent) => {
+  for (const change of event.documentChanges) {
+    if (
+      change.type === "PROPERTY_CHANGE" &&
+      change.properties.includes("fontSize")
+    ) {
+      const node = change.node;
+      if (node.removed) {
+        continue;
+      }
+      const artboard = getArtboard(node);
+      if (!artboard) {
+        continue;
+      }
+      const artboardData = getResponsiveArtboardData(artboard);
+      if (!artboardData) {
+        continue;
+      }
+
+      console.log("fontSize changed", node, artboard);
+    }
+  }
 };
 
 figma.on("documentchange", onDocumentChange);
